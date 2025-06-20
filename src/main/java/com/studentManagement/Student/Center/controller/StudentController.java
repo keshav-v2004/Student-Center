9a1e1ce3568bb4295436e3c3dbd854af87241e9f
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2025-06-20 20:48:40 +0530

    added response entity logic

--- src/main/java/com/studentManagement/Student/Center/controller/StudentController.java
+++ src/main/java/com/studentManagement/Student/Center/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.studentManagement.Student.Center.controller;
 import com.studentManagement.Student.Center.entity.Student;
 import com.studentManagement.Student.Center.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,31 +16,31 @@ public class StudentController {
 
     //get all student
     @GetMapping("/allStudent")
-    public List<Student> getAllStudent(){
+    public ResponseEntity<?> getAllStudent(){
         return studentService.getAllStudents();
     }
 
     //get student by id
     @GetMapping("/getById/{myId}")
-    public Student getStudentById(@PathVariable String myId){
+    public ResponseEntity<?> getStudentById(@PathVariable String myId){
         return studentService.getStudentById(myId);
     }
 
     // add new student
     @PostMapping("/addNewStudent")
-    public void addNewStudent(@RequestBody Student student){
-        studentService.addStudent(student);
+    public ResponseEntity<?> addNewStudent(@RequestBody Student student){
+        return studentService.addStudent(student);
     }
 
     //update a student by id
     @PutMapping("/updateById/{myId}")
-    public boolean updateById(@PathVariable String myId , @RequestBody Student student){
+    public ResponseEntity<?> updateById(@PathVariable String myId , @RequestBody Student student){
         return studentService.updateStudent(myId, student);
     }
 
     //delete student
     @DeleteMapping("/deleteStudentById/{myId}")
-    public boolean deleteById(@PathVariable String myId){
+    public ResponseEntity<?> deleteById(@PathVariable String myId){
         return studentService.deleteStudent(myId);
     }
 
--- src/main/java/com/studentManagement/Student/Center/service/StudentService.java
+++ src/main/java/com/studentManagement/Student/Center/service/StudentService.java
@@ -1,10 +1,14 @@
 package com.studentManagement.Student.Center.service;
 
 import com.studentManagement.Student.Center.entity.Student;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class StudentService {
@@ -13,39 +17,61 @@ public class StudentService {
     public List<Student> studentList = new ArrayList<>();
 
     //get all student
-    public List<Student> getAllStudents() {
-        return studentList;
+    public ResponseEntity<?> getAllStudents() {
+        if (studentList.isEmpty()){
+            return new ResponseEntity<>("no students added" , HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(studentList , HttpStatus.OK);
     }
 
     //get student by id
-    public Student getStudentById(String id) {
+    public ResponseEntity<?> getStudentById(String id) {
 
-        return studentList.stream()
+        Optional<Student> x = studentList.stream()
                 .filter(student -> student.getRoll().equals(id))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
+        if (x.isPresent()){
+            return new ResponseEntity<>(x,HttpStatus.OK);
+        }
+        else return new ResponseEntity<>("no such student with supplied id present",HttpStatus.NOT_FOUND);
+
     }
 
     // add new student
-    public void addStudent(Student student) {
-        studentList.add(student);
+    public ResponseEntity<?> addStudent(Student student) {
+        try {
+            studentList.add(student);
+            return new ResponseEntity<>("successfully added", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage() , HttpStatus.BAD_REQUEST);
+        }
     }
 
     //update a student by id
-    public boolean updateStudent(String id, Student updatedStudent) {
+    public ResponseEntity<?> updateStudent(String id, Student updatedStudent) {
         for (int i = 0; i < studentList.size(); i++) {
             Student s = studentList.get(i);
             if (s.getRoll().equals(id)) {
                 studentList.set(i, updatedStudent);
-                return true;
+                return new ResponseEntity<>("updated successfully" , HttpStatus.OK);
             }
         }
-        return false;
+        return new ResponseEntity<>("Something went wrong" , HttpStatus.BAD_REQUEST);
     }
 
     //delete student
-    public boolean deleteStudent(String id) {
-        return studentList.removeIf(student -> student.getRoll().equals(id));
+    public ResponseEntity<?> deleteStudent(String id) {
+
+        try {
+            boolean isRemoved = studentList.removeIf(student -> student.getRoll().equals(id));
+            if (isRemoved){
+                return new ResponseEntity<>("deleted successfully" , HttpStatus.OK);
+            }
+            else return new ResponseEntity<>("no student with supplied id present" , HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {}
+
+        return new ResponseEntity<>("something went wrong" , HttpStatus.BAD_REQUEST);
+
     }
 
 
