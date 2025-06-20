e7a1433ad32d753762e765098322ba1e0c969ead
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2025-06-20 19:02:38 +0530

    initial commit

---
+++ src/test/java/com/studentManagement/Student/Center/StudentCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentManagement.Student.Center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
