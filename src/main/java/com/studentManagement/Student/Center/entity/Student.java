91253e847f4c98ab07dc5643774e46ac8e3f9496
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2025-06-20 20:21:31 +0530

    lombok configuration added

--- src/main/java/com/studentManagement/Student/Center/entity/Student.java
+++ src/main/java/com/studentManagement/Student/Center/entity/Student.java
@@ -1,48 +1,15 @@
 package com.studentManagement.Student.Center.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Student {
 
     private String roll;
     private String name;
     private String branch;
-
-
-    //constructor
-    public Student(
-            String roll, String name, String branch
-    ) {
-        this.roll = roll;
-        this.name = name;
-        this.branch = branch;
-    }
-
-    public String getRoll() {
-        return roll;
-    }
-
-    public void setRoll(String roll) {
-        this.roll = roll;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBranch() {
-        return branch;
-    }
-
-    public void setBranch(String branch) {
-        this.branch = branch;
-    }
-
-
-
 
 
 }
