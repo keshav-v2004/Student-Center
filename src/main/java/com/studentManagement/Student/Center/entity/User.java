6c965bae797711ef100cc31af32ec60bd95ffb74
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2025-07-07 18:32:23 +0530

    User entity

---
+++ src/main/java/com/studentManagement/Student/Center/entity/User.java
@@ -0,0 +1,27 @@
+package com.studentManagement.Student.Center.entity;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password; // stored as hashed (BCrypt)
+}
