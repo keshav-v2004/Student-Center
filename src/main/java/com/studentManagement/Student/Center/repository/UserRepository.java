1f51e09ac8bd985321598266dfe5cf0c47c91d97
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2025-07-07 18:32:34 +0530

    User repository

---
+++ src/main/java/com/studentManagement/Student/Center/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.studentManagement.Student.Center.repository;
+
+import com.studentManagement.Student.Center.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+}
